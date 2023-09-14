76613f0cb788d170df16433712bbd9f1557ed4fa
Felipe Simões <felipebouabci@gmail.com>
2023-09-13 21:48:06 -0300

    correção de bugs e melhorias estéticas

--- scripts/GradienteVerde.gd
+++ scripts/GradienteVerde.gd
@@ -3,8 +3,10 @@ extends Sprite
 
 func travar():
 	var tween := create_tween()
+# warning-ignore:return_value_discarded
 	tween.tween_property(self, "modulate", Color.transparent, 0.3)
 
 func destravar():
 	var tween := create_tween()	
+# warning-ignore:return_value_discarded
 	tween.tween_property(self, "modulate", Color.white, 0.7)
--- scripts/MenuInicial.gd
+++ scripts/MenuInicial.gd
@@ -1,6 +1,7 @@
 extends Control
 
 func MudarCenaPasteur():
+# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://cenas/CenárioPasteur.tscn")
 
 
