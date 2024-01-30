e258d0c29a73d1f22e64c6a5d4253e553202c68a
Felipe Simões <felipebouabci@gmail.com>
2024-01-29 21:26:23 -0300

    avançando no tutorial

--- scripts/Tutorial.gd
+++ scripts/Tutorial.gd
@@ -2,7 +2,11 @@ extends Control
 
 var etapas = [
 	{
-		"texto": "Bem vindo, filho da puta!",
+		"texto": "Bem vindo ao seu laboratório.\nVamos aprender a usar este simulador?\nAperte em avançar",
+		"destacados": [],
+	},
+	{
+		"texto": "Vamos começar adicionando um frasco.\nAperte no botão \"+\" na bancada\nQuando conseguir clique em \"avançar\"",
 		"destacados": ["%Bancada"],
 	}
 ]
@@ -10,6 +14,8 @@ var etapa_atual := 0
 var todos_nos = []
 var hipotese = ""
 signal AtualizaHipotese(texto)
+
+var parents = {}
 
 func _ready():
 	obterFilhos($".")
@@ -21,11 +27,18 @@ func _ready():
 	TextoAcessivel(MenuAcessibilidade.texto_acessivel)
 	
 func defineEtapa(etapa: int):
-	$Node2D/Label.text = etapas[etapa].texto
-	for item in todos_nos:
-		item.z_index = 10
+	get_parent().move_child($Node2D,1)
+	$Node2D/texto_do_tutorial.text = etapas[etapa].texto
+	for node in etapas[etapa_atual].destacados:
+		var no = get_node(node)
+		no.queue_free()
+		parents[no].add_child(no)
 	for node in etapas[etapa].destacados:
-		get_node(node).z_index = 601
+		var no = get_node(node)
+		parents[no] = no.get_parent()
+		no.get_parent().remove_child(no)
+		$Node2D/vemca.add_child(no)
+	etapa_atual = etapa
 
 func obterFilhos(ref):
 	for node in ref.get_children():
@@ -34,6 +47,11 @@ func obterFilhos(ref):
 		else:	
 			if ref.get_child_count() > 0:	
 				obterFilhos(node)
+				
+func _on_avancar() -> void:
+	defineEtapa(etapa_atual + 1)
+	if etapa_atual == len(etapas)-1:
+		$Node2D/avancar.disabled = true
 
 
 func VoltarMenu():
@@ -57,5 +75,4 @@ func TextoAcessivel(b):
 		self.theme = load("res://fontes-e-temas/renascença.tres")
 		$"%Popups".theme = load("res://fontes-e-temas/renascença.tres")
 		$Caderno.rotation_degrees = -0.8
-		
-		
+
